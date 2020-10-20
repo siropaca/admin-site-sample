ca50548622ce02d83264b596a43776c2fd31d31a
shirai ryohei <ryhshr3@gmail.com>
2020-10-20 22:10:30 +0900

    material-uiをSSR用に修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
@@ -8,15 +10,30 @@ import { ThemeProvider } from '@material-ui/core/styles';
 
 import ResetStyle from '../styles/reset';
 import GlobalStyle from '../styles/global';
-
 import theme from '../styles/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <React.StrictMode>
-      <ResetStyle />
-      <GlobalStyle />
+    <React.Fragment>
+      <Head>
+        <title>BortShare Management</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
       <ThemeProvider theme={theme}>
+        <ResetStyle />
+        <GlobalStyle />
         {true ? (
           <Layout>
             <Component {...pageProps} />
@@ -25,8 +42,11 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         )}
       </ThemeProvider>
-    </React.StrictMode>
+    </React.Fragment>
   );
 }
 
-export default MyApp;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired
+};
